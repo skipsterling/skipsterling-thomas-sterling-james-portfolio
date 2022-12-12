63746645df8af005dace21193c609cbdb1e2ed51
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-12-13 07:50:48 +1000

    about me section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+      <section>
+        <div className="about">
+  <h3>Hello, I'm</h3>
+  <h1>THOMAS</h1>
+  <p>I’m a 35 year old male from Burleigh Heads on the Gold Coast. Currently I am working as a Content Creator, however I am looking to move into the Software / Web engineering space to further my skills  in the industry. Whether I’m working in a team or as an individual I'm always happy, positive and passionate about the task at hand. My strong points are my level of creativity, self and time management skills and drive to stay up to date with emerging technologies.  </p>
+  </div>
+</section>
       <div class="outterCircle">
   <div class="innerCircle">
     </div>
