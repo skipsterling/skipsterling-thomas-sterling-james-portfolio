01e299d6e642107b46c0731ee431520fa23b271e
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-12-12 19:09:17 +1000

    Backgorund css set up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  
+      <div className="container">
+      <div class="outterCircle">
+  <div class="innerCircle">
+    </div>
+  </div>
+</div>
     </div>
   );
 }
