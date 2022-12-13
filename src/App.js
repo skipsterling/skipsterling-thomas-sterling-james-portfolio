46d28d611666dd1e842a33d29a6b3fb5c96eca04
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-12-13 17:33:28 +1000

    new layout created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-      <section>
-        <div className="about">
-  <h3>Hello, I'm</h3>
-  <h1>THOMAS</h1>
-  <p>I’m a 35 year old male from Burleigh Heads on the Gold Coast. Currently I am working as a Content Creator, however I am looking to move into the Software / Web engineering space to further my skills  in the industry. Whether I’m working in a team or as an individual I'm always happy, positive and passionate about the task at hand. My strong points are my level of creativity, self and time management skills and drive to stay up to date with emerging technologies.  </p>
-  </div>
-</section>
-      <div class="outterCircle">
-  <div class="innerCircle">
-    </div>
-  </div>
+return (
+<div className="App">
+<div className="container">
+<div className="about"><h3>Hello, I'm</h3>
+<h1>THOMAS</h1>
+<p>A 35 year old male from Burleigh Heads. I am currently working as a Content Creator, however after completing a certificate in Full Stack Development at The University of Adelaide, I am looking to move into the Software / Web engineering space to further my skills in the industry. </p>
+<div className="IconContainer" data-speed='3'>
+<div className="gh" data-speed='1'>GH</div>
+<div className="li" data-speed='2'>LI</div>
 </div>
-    </div>
-  );
+</div>
+<div className="box2"></div>
+<div className="box3"></div>
+</div>
+</div>
+);
 }
 
 export default App;
