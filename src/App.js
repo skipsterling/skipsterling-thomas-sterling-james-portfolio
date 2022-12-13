f318fb442d7d0c666356d959d99acbb0bf6327a8
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-12-13 18:23:14 +1000

    git linkedin and email icons added with hover effect

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ return (
 <h1>THOMAS</h1>
 <p>A 35 year old male from Burleigh Heads. I am currently working as a Content Creator, however after completing a certificate in Full Stack Development at The University of Adelaide, I am looking to move into the Software / Web engineering space to further my skills in the industry. </p>
 <div className="IconContainer" data-speed='3'>
-<div className="gh" data-speed='1'>GH</div>
-<div className="li" data-speed='2'>LI</div>
+<div className="gh"><i class="fa-brands fa-github"></i></div>
+<div className="li"><i class="fa-brands fa-linkedin"></i></div>
+<div className="email"><i class="fa-regular fa-envelope"></i></div>
 </div>
 </div>
 <div className="box2"></div>
