9e40e942e9931c1096e926b2a5b47ace7ef1e629
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-12-15 17:33:42 +1000

    added images to cards and changed layout

--- src/components/Parallax/index.js
+++ src/components/Parallax/index.js
@@ -1,129 +1,68 @@
 import {
     MouseParallaxChild,
     MouseParallaxContainer
-    } from "react-parallax-mouse";
-    
-    import img1 from '../../assets/images/react.png';
-    import img2 from '../../assets/images/mdb.png';
-    import img3 from '../../assets/images/njs.png';
-    import img4 from '../../assets/images/exp.png';
-    
-    export default function parallax() {
+  } from "react-parallax-mouse";
+  import "./styles.css";
+  import img1 from '../../assets/images/react.png';
+  import img2 from '../../assets/images/mdb.png';
+import img3 from '../../assets/images/njs.png';
+import img4 from '../../assets/images/exp.png';
+
+  export default function parallax() {
     return (
-    <>
-    <div
-    style={{
-    height: "50%",
-    background: "#f2f4f5"
-    }}
-    >
-    <div
-    style={{
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    }}
-    >
-    <MouseParallaxContainer
-    className="parallax"
-    containerStyle={{
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    }}
-    globalFactorX={0.3}
-    globalFactorY={0.3}
-    resetOnLeave
-    >
-    <MouseParallaxChild
-    factorX={0.6}
-    factorY={0.1}
-    style={{
-    backgroundPositionY: "50%",
-    transform: "scale(1.2)",
-    position: "absolute",
-    filter: "blur(4px) brightness(50%)",
-    backgroundSize: "auto",
-    backgroundRepeat: "repeat",
-    width: "100%",
-    height: "100%",
-    backfaceVisibility: "hidden"
-    }}
-    />
-    <MouseParallaxChild
-    factorX={0.5}
-    factorY={0.5}
-    style={{
-    filter: "invert(1)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%"
-    }}
-    >
-    <img height="50%" src={img1} alt="" />
-    </MouseParallaxChild>
-    <MouseParallaxChild
-    factorX={0.7}
-    factorY={0.5}
-    style={{
-    filter: "invert(1)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%"
-    }}
-    >
-    <img height="50%" src={img2} alt="" />
-    </MouseParallaxChild>
-    <MouseParallaxChild
-    factorX={0.9}
-    factorY={0.5}
-    style={{
-    filter: "invert(1)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%"
-    }}
-    >
-    <img height="50%" src={img3} alt="" />
-    </MouseParallaxChild>
-    <MouseParallaxChild
-    factorX={0.7}
-    factorY={0.5}
-    style={{
-    filter: "invert(1)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%"
-    }}
-    >
-    <img height="50%" src={img4} alt="" />
-    </MouseParallaxChild>
-    <MouseParallaxChild
-    factorX={0.5}
-    factorY={0.5}
-    style={{
-    filter: "invert(1)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "auto",
-    height: "100%"
-    }}
-    >
-    </MouseParallaxChild>
-    </MouseParallaxContainer>
-    </div>
-    </div>
-    </>
+      <>
+        <div className="d1">
+          <div className="d2">
+          <div className="innerContainer">
+            <MouseParallaxContainer
+              className="parallax"
+              globalFactorX={0.3}
+              globalFactorY={0.3}
+              resetOnLeave
+            >
+             
+              <MouseParallaxChild
+              className="paraChild"
+                factorX={0.6}
+                factorY={0.1}/>
+
+              <MouseParallaxChild
+              className="paraChild1"
+                factorX={0.5}
+                factorY={0.5}>
+                <img className="img1" height="50%" src={img1} alt="" />
+              </MouseParallaxChild>
+
+              <MouseParallaxChild
+              className="paraChild2"
+                factorX={0.7}
+                factorY={0.5}>
+                <img height="50%" src={img2} alt="" />
+              </MouseParallaxChild>
+
+              <MouseParallaxChild
+              className="paraChild3"
+                factorX={0.9}
+                factorY={0.5}>
+                <img height="50%" src={img3} alt="" />
+              </MouseParallaxChild>
+
+              <MouseParallaxChild
+              className="paraChild4"
+                factorX={0.7}
+                factorY={0.5}>
+                <img height="50%" src={img4} alt="" />
+              </MouseParallaxChild>
+              <MouseParallaxChild
+                factorX={0.5}
+                factorY={0.5}>
+               
+              </MouseParallaxChild>
+             
+            </MouseParallaxContainer>
+            </div>
+          </div>
+        </div>
+      </>
     );
-    }
+  }
--- src/components/ThreeSectionTwo/index.js
+++ src/components/ThreeSectionTwo/index.js
@@ -1,42 +1,51 @@
 import React from 'react';
 import './styles.css';
-import img1 from '../../assets/images/react.png';
 
 function threeSecTwo() {
-return (
-<div className="App">
-<div className="container">
-<div className="cardOne">
-{/* Image is just a placeholder */}
-<img src={img1} alt="react"/>
-<h3 className="c1h3">Content Creator Portfolio</h3>
-<div className="buttonContainer">
-<button className="codeButton">Code</button>
-<button className="appButton">App</button>
-</div>
-</div>
-<div className="cardTwo">
-<img src={img1} alt="react"/>
-<h3 className="c1h3">Weather API</h3>
-<div className="buttonContainer">
-<button className="codeButton">Code</button>
-<button className="appButton">App</button>
-</div>
-</div>
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="cardOne" >
+                    {/* Image is just a placeholder */}
 
-<div className="cardThree">
-<img src={img1} alt="react"/>
-<h3 className="c1h3">Day Scheduler</h3>
-<div className="buttonContainer">
-<button className="codeButton">Code</button>
-<button className="appButton">App</button>
-</div>
-</div>
-</div>
-</div>
+                    <h3 className="c1h3">Content Creator Portfolio</h3>
+                    <div className="buttonContainer">
+                     
+                        <a href="https://www.visualsbythomas.com" target="_blank" rel="noreferrer"> 
+                        <button className="appButton">App</button></a>
+                    </div>
+                </div>
+                <div className="cardTwo">
+                    <h3 className="c1h3">Weather API</h3>
+
+                    <div className="buttonContainer">
+                   
+                        <a href="https://skipsterling.github.io/homework-week6/" target="_blank" rel="noreferrer"> 
+                        <button className="appButton">App</button></a>
+                    </div>
+                </div>
+
+                <div className="cardThree">
+                    <h3 className="c1h3">Day Scheduler</h3>
+                    <div className="buttonContainer">
+                    <a href=" https://skipsterling.github.io/homework-week5/" target="_blank" rel="noreferrer"> 
+                        <button className="appButton">App</button></a>
+                    </div>
+                </div>
+               
+                <div className="cardFour">
+                    <h3 className="c1h3">Client Website</h3>
+                    <div className="buttonContainer">
+                    <a href=" https://www.rjpodiatry.com.au/" target="_blank" rel="noreferrer"> 
+                        <button className="appButton">App</button></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
 
-);
+    );
 }
 
 export default threeSecTwo;
