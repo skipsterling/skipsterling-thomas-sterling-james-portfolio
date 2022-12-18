5b4d5a69f0998ab1ea4c84492cbf1dbcd0384865
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-12-18 15:42:33 +1000

    stying added and Profile pic

--- src/components/ThreeSection/index.js
+++ src/components/ThreeSection/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles.css';
 import Parallax from '../Parallax';
+import test from '../../assets/images/test.png'
 
 function threeSec() {
 return (
-<div className="App">
+<div className="main">
 <div className="container">
 <div className="about"><h3>Hello, I'm</h3>
 <h1>THOMAS</h1>
@@ -14,7 +15,9 @@ return (
 <div className="li"><a href="https://www.linkedin.com/in/tom-james-4893b2120/"><i class="fa-brands fa-linkedin"></i></a></div>
 <div className="email"><a href="https://docs.google.com/document/d/1YjPb1Y0DWoKn67EPxSNaBEMSUznZqhgfkuiJ0_xLVCY/edit"><i class="fa-regular fa-envelope"></i></a></div></div>
 </div>
-<div className="box2"></div>
+<div className="box2">
+<img className='testImg'src={test} alt={test}/>
+</div>
 
 <div className="box3">
 <Parallax />
